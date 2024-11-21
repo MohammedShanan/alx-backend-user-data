dfe82aca5177a7bfe464384f1a7ef7786c3c2592
mohammedshanan <mohammedshana12@gmail.com>
2024-11-21 14:31:01 +0200

    updated auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -40,14 +40,11 @@ def login() -> str:
     Return:
         - The account login payload.
     """
-    email, password = request.form.get("email"), request.form.get("password")
+    email = request.form.get("email")
+    password = request.form.get("password")
     if not AUTH.valid_login(email, password):
         abort(401)
     session_id = AUTH.create_session(email)
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
     return response
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
